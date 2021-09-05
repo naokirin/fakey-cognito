1d1fedc7d0c8f14a5b9f3ea1299483e0f697d235
naokirin <naoki.rin186@gmail.com>
2021-09-06 04:28:32 +0900

    Construct a simple web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+const DEFAULT_LOG_LEVEL: &str = "debug";
+const LOG_LEVEL_KEY: &str = "RUST_LOG";
+
+fn setup_logger() {
+    let log_level = std::env::var(LOG_LEVEL_KEY)
+        .unwrap_or(DEFAULT_LOG_LEVEL.to_string());
+    std::env::set_var(LOG_LEVEL_KEY, log_level);
+    pretty_env_logger::init();
+}
+
+#[tokio::main]
+async fn main() {
+    setup_logger();
+
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}", name))
+        .with(warp::log("debug"));
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 8080))
+        .await;
 }
