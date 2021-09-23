935cf3b480041f42bdf1ad8c9f90018c50591198
naokirin <naoki.rin186@gmail.com>
2021-09-23 12:30:36 +0900

    Add AdminDisableProviderForUserTest

--- tests/user_pools/mod.rs
+++ tests/user_pools/mod.rs
@@ -3,8 +3,9 @@ use fakey_cognito::*;
 mod admin_add_user_to_group_test;
 mod admin_confirm_sign_up_test;
 mod admin_create_user_test;
-mod admin_delete_user_test;
 mod admin_delete_user_attributes_test;
+mod admin_delete_user_test;
+mod admin_disable_provider_for_user_test;
 
 pub async fn setup() {
     opts::init_opt().await;
