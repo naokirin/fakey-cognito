8baebe8c2756441938803150b4ace8ab1dcdcd21
naokirin <naoki.rin186@gmail.com>
2021-12-22 20:59:41 +0900

    Update crates

--- src/hooks.rs
+++ src/hooks.rs
@@ -1,4 +1,4 @@
-use heck::SnakeCase;
+use heck::AsSnakeCase;
 use pyo3::prelude::*;
 use pyo3::types::PyList;
 use std::path::Path;
@@ -13,7 +13,7 @@ pub fn call_request_hook<T>(
 where
     T: serde::Serialize,
 {
-    let pyname = action.to_snake_case();
+    let pyname = AsSnakeCase(action);
     let dir = hooks_dir.unwrap_or(DEFAULT_HOOK_DIR_PATH);
     let path = format!("{}/{}.py", dir, pyname);
     if !Path::new(&path).exists() {
@@ -32,7 +32,7 @@ where
         // syspath.insert(0, path.display()).unwrap();
         syspath.insert(0, dir).unwrap();
 
-        let hook = py.import(action.to_snake_case().as_str())?;
+        let hook = py.import(format!("{}", AsSnakeCase(action)).as_str())?;
         let arg = serde_json::to_string(value).unwrap_or_else(|_| "".to_string());
         hook.getattr("hook")?.call1((arg,))?.extract()
     })
